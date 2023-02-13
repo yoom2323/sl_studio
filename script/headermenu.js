6a20b46305dc1c438498ffeac7d93f4475400ce5
yoom2323 <omgvic@naver.com>
2023-02-13 18:00:05 +0900

    230213 헤더작업

--- script/headermenu.js
+++ script/headermenu.js
@@ -1,8 +1,11 @@
 // 스크롤바 없애기
 function hidden() {
-  document.body.style.overflow = "hidden";
+  document.body.style.overflowY = "hidden";
 }
-
+// 스크롤바 보이기
+function scroll() {
+  document.body.style.overflowY = "scroll";
+}
 const shareBtn = document.querySelector(".xi-share-alt-o");
 const share = document.querySelector("#share");
 
@@ -16,6 +19,8 @@ searchBtn.addEventListener("click", () => search.classList.toggle("active"));
 
 searchX.addEventListener("click", () => search.classList.toggle("active"));
 
+//메뉴 토글 슬라이드
+
 const menuBtn = document.querySelector(".fa-bars");
 const menu = document.querySelector(".gnbWrapper");
 const gnbX = document.querySelector("#gnbX");
@@ -25,27 +30,45 @@ menuBtn.addEventListener("click", function () {
   menu.classList.toggle("active");
 });
 gnbX.addEventListener("click", function () {
-  hidden(false);
+  scroll(true);
   menu.classList.toggle("active");
 });
 
 const about = document.querySelector("#about");
 const aboutLi = document.querySelector("#aboutLi");
+const abPlus = document.querySelector(".abPlus");
 
 about.addEventListener("click", function () {
   aboutLi.classList.toggle("active");
+  archiveLi.classList.remove("active");
+  noticeLi.classList.remove("active");
+  abPlus.classList.toggle("active");
+  arPlus.classList.remove("active");
+  noPlus.classList.remove("active");
 });
 
 const archive = document.querySelector("#archive");
 const archiveLi = document.querySelector("#archiveLi");
+const arPlus = document.querySelector(".arPlus");
 
-archive.addEventListener("click", function () {
+const archiveToggle = archive.addEventListener("click", function () {
   archiveLi.classList.toggle("active");
+  noticeLi.classList.remove("active");
+  aboutLi.classList.remove("active");
+  arPlus.classList.toggle("active");
+  abPlus.classList.remove("active");
+  noPlus.classList.remove("active");
 });
 
 const notice = document.querySelector("#notice");
 const noticeLi = document.querySelector("#noticeLi");
+const noPlus = document.querySelector(".noPlus");
 
 notice.addEventListener("click", function () {
   noticeLi.classList.toggle("active");
+  archiveLi.classList.remove("active");
+  aboutLi.classList.remove("active");
+  noPlus.classList.toggle("active");
+  abPlus.classList.remove("active");
+  arPlus.classList.remove("active");
 });
