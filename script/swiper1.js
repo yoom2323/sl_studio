9245d223006c2720974fc242384d720857ede796
yoom2323 <omgvic@naver.com>
2023-02-17 12:42:53 +0900

    스와이퍼 작업 완료

--- script/swiper1.js
+++ script/swiper1.js
@@ -1,12 +1,27 @@
 const mainSwiper = new Swiper("#mainSwiper", {
   parallax: true,
   navigation: {
-    nextEl: ".swiper-arrow1",
-    prevEl: ".swiper-arrow2",
+    nextEl: ".swiper-arrow2",
+    prevEl: ".swiper-arrow1",
   },
+
   loop: true,
   pagination: {
     el: ".pagination1",
     type: "fraction",
   },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+const prevBtn = document.querySelector(".prevBtn");
+const nextBtn = document.querySelector(".nextBtn");
+
+prevBtn.addEventListener("click", function (e) {
+  mainSwiper.slidePrev();
+});
+nextBtn.addEventListener("click", function (e) {
+  mainSwiper.slideNext();
 });
